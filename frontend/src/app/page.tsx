57c77cd8e1c4616e2e0f55378fb145db24fbd56c
mateofu <mateofuentes@utp.edu.co>
2024-11-13 22:12:43 -0500

    Mover contenido de out a la raíz

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,6 @@
-
 import React from "react";
 
-const Page = ({ children }: { children: React.ReactNode }) => {
+const Page = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       cldmcldmlcm
